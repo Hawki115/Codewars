05a82e2903aa2fcedad1045e7d5e2d1dea964b83
Connor <connorhawkins19@icloud.com>
2025-05-27 17:36:27 +0100

    Add files via upload

---
+++ #Beginner reduce but grow.py
@@ -0,0 +1,9 @@
+#Beginner reduce but grow
+def grow(arr):
+    produce = 1
+    for num in arr:
+        produce *= num
+    return produce
+
+    
+   pass
---
+++ #Fake Binary.py
@@ -0,0 +1,9 @@
+#Fake Binary
+def fake_bin(x):
+    result = ''
+    for char in x:
+        if int(char) <5:
+            result += '0'
+        else:
+            result += '1'
+    return result
